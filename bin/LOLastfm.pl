093f01ad8a59dead03b0b7ffa4cbf081a2b26dff
meh <meh.ffff@gmail.com>
2009-11-25 12:21:42 +0100

    Added the license.

--- bin/LOLastfm.pl
+++ bin/LOLastfm.pl
@@ -1,10 +1,23 @@
 #! /usr/bin/perl
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 use strict;
 use warnings;
 use utf8;
 use Getopt::Std;
 use XML::Simple qw(:strict);
-use Net::LastFM::Submission qw(encode_data);
+use Net::LastFM::Submission;
 
 my $Version = '0.1';
 
@@ -15,7 +28,7 @@ getopts('u:p:P:f:c:h', \%options);
 
 if ($options{h}) {
     print Misc::usage();
-    exit(0);
+    exit;
 }
 
 my $Config = XMLin($options{f} || '/etc/LOLastfm.xml', KeyAttr => 1, ForceArray => 0);
